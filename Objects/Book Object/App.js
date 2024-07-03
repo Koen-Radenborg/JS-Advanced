d4629868eebe8bb29ffe875114366295c35e3278
Koen-Radenborg <9021648@student.zadkine.nl>
2024-07-03 14:25:09 +0200

    Book Object

--- Objects/Book Object/App.js
+++ Objects/Book Object/App.js
@@ -1,4 +1,7 @@
-const book = {
+const output = document.querySelector(".output");
+let out = ``;
+
+let book = {
     title: "Art of War",
     author: "Sun Tzu",
     publisher: "Sun Tzu",
@@ -6,3 +9,12 @@ const book = {
     amountSold: 23858329583295,
     price: "7 euro"
 };
+
+out = `<h2>${book.title}</h2>
+<p>Author: ${book.author}</p>
+<p>Publisher: ${book.publisher}</p>
+<p>Year: ${book.year}</p>
+<p>Amount sold: ${book.amountSold}</p>
+<p>Price: ${book.price}</p>`
+
+output.innerHTML = out
